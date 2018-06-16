a945f0e57b492b55d34746deb76417c4831fe512
Drew McLean <drewtoid@gmail.com>
2018-06-16 11:47:00 -0500

    Added networking function for API GET requests.

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -12,16 +12,26 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.client.ResponseHandler;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    // https://apiv2.bitcoinaverage.com/indices/{symbol_set}/ticker/{symbol}
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/";
+
 
     // Member Variables:
     TextView mPriceTextView;
@@ -46,33 +56,48 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO: Set an OnItemSelected listener on the spinner
 
+        //HashMap<String,String> tickerRequest = new HashMap<>();
+       // tickerRequest.put("symbol_set","global");
+        //tickerRequest.put("symbol","BTCUSD");
+        //letsDoSomeNetworking(tickerRequest);
+
     }
 
     // TODO: complete the letsDoSomeNetworking() method
-    private void letsDoSomeNetworking(String url) {
+    private void letsDoSomeNetworking(HashMap<String,String> request) {
+        // https://apiv2.bitcoinaverage.com/indices/global/ticker/{ticker}
+//    // https://apiv2.bitcoinaverage.com/indices/{symbol_set}/ticker/{symbol}
+//    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/";
 
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // called when response HTTP status is "200 OK"
-//                Log.d("Clima", "JSON: " + response.toString());
-//                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
-//                updateUI(weatherData);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-//                Log.d("Clima", "Request fail! Status code: " + statusCode);
-//                Log.d("Clima", "Fail response: " + response);
-//                Log.e("ERROR", e.toString());
-//                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        String url = BASE_URL + request.get("symbol_set") + "/ticker/" + request.get("symbol");
+        Log.d("BitcoinTicker",url);
+        AsyncHttpClient client = new AsyncHttpClient();
 
+        client.get(url, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
 
+                try {
+                    String bitcoinAsk = response.getString("ask");
+                    Log.d("BitcoinTicker","GET success! Latest ask price is " + bitcoinAsk);
+                    mPriceTextView.setText(bitcoinAsk);
+                }
+                catch(Throwable e)
+                {
+                    Log.d("BitcoinTicker","JSONObject incorrect!");
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+
+                Log.d("BitcoinTicker","GET failed!");
+
+            }
+        });
     }
 
 
